be037de8d8a08e66e2d11bbd96d18412eec0a840
James Kekeli <jameskekeli12@gmail.com>
2026-04-10 23:19:38 +0000

    feat:API data rendering with DRF

---
+++ product/api/viewsets.py
@@ -0,0 +1,16 @@
+'''Documentation String'''
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.parsers import MultiPartParser,FormParser
+from django_filters.rest_framework import DjangoFilterBackend
+from product.serializers import ProductSerializer #pylint:disable=e0401
+from product.models import Product #pylint:disable=e0401
+
+class ProductViewset(ModelViewSet):
+    '''Documentation String'''
+    queryset = Product.objects.select_related("category","market").all()
+
+    serializer_class = ProductSerializer
+    parser_classes = (MultiPartParser,FormParser)
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {"market__slug":["exact"]}
