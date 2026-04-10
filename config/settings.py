ea76ead306c5c4a437bd18ca9d287c798c028d52
James Kekeli <jameskekeli12@gmail.com>
2026-04-10 23:09:04 +0000

    feat:including DRF to settings

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'category',
     'product',
     'account',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 
 # Unfold Settings
